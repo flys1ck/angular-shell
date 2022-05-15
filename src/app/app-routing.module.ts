52bdc8e2843f33e8bd6201090e10c40b23c9fb4a
Gregor Zweig <gregor.zweig@memsource.com>
2022-05-15 13:51:35 +0200

    feat: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShellDashboardComponent } from './shell-dashboard/shell-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: ShellDashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
