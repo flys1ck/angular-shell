bd6066de05d74765ab2cf239b2e74e8413b2efae
Gregor Zweig <zwickzwackzwieback@gmx.de>
2022-06-08 14:11:42 +0200

    config: use dynamic host configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,14 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const mf = require("@angular-architects/module-federation/webpack");
-const path = require("path");
-const share = mf.share;
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
-const sharedMappings = new mf.SharedMappings();
-sharedMappings.register(path.join(__dirname, "tsconfig.json"), [
-  /* mapped paths to share */
-]);
-
-module.exports = {
-  output: {
-    uniqueName: "angularShell",
-    publicPath: "auto",
-  },
-  optimization: {
-    runtimeChunk: false,
-  },
-  resolve: {
-    alias: {
-      ...sharedMappings.getAliases(),
-    },
-  },
-  experiments: {
-    outputModule: true,
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      library: { type: "module" },
-
-      remotes: {},
-
-      shared: share({
-        "@angular/core": {
-          singleton: true,
-          strictVersion: true,
-          requiredVersion: "auto",
-        },
-        "@angular/common": {
-          singleton: true,
-          strictVersion: true,
-          requiredVersion: "auto",
-        },
-        "@angular/common/http": {
-          singleton: true,
-          strictVersion: true,
-          requiredVersion: "auto",
-        },
-        "@angular/router": {
-          singleton: true,
-          strictVersion: true,
-          requiredVersion: "auto",
-        },
-
-        ...sharedMappings.getDescriptors(),
-      }),
+module.exports = withModuleFederationPlugin({
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
     }),
-    sharedMappings.getPlugin(),
-  ],
-};
+  },
+});
